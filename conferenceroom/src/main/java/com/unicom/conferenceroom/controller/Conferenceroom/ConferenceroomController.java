a6a8de368dc6b063cc3d66df4e98449df55dcc15
linanali0606 <745245071@qq.com>
2020-09-20 20:09:25 +0800

    Update ConferenceroomController.java

--- conferenceroom/src/main/java/com/unicom/conferenceroom/controller/Conferenceroom/ConferenceroomController.java
+++ conferenceroom/src/main/java/com/unicom/conferenceroom/controller/Conferenceroom/ConferenceroomController.java
@@ -17,35 +17,36 @@ public class ConferenceroomController {
     private ConferenceroomService conferenceroomService;
 
 
-    @RequestMapping(value = "/conferenceroom/{id}",method=RequestMethod.GET)
-    public String selectConferenceroomByid(@PathVariable("id") int id){
-        Conferenceroom conferenceroom=conferenceroomService.selectConferenceroomByid(id);
-        return JSON.toJSONString(conferenceroom);
+    @RequestMapping(value = "/selectById")
+    public Conferenceroom selectConferenceroomByName(String roomname){
+        Conferenceroom conferenceroom=conferenceroomService.selectConferenceroomByName("李娜");
+        return conferenceroom;
     }
+    
     @RequestMapping(value="/conferenceroom",method=RequestMethod.GET)
     public String getAllConferencerooms(){
         return JSON.toJSONString(conferenceroomService.getAllConferencerooms());
 
     }
 
-
-
-    @RequestMapping(value = "/meetingroom_add",method = RequestMethod.POST)
+  @RequestMapping(value = "/meetingroom_add",method = RequestMethod.POST)
     public int insertConferenceroom(@RequestBody Conferenceroom conferenceroom){
         return  conferenceroomService.insertConferenceroom(conferenceroom);
     }
 
 
 
-    @RequestMapping(value="/meetingroom_update",method = RequestMethod.POST)
-    public  int updateConferenceroom(@RequestBody Conferenceroom conferenceroom){
-        return conferenceroomService.updateConferenceroom(conferenceroom);
+    @RequestMapping(value="/meetingroom_update",method = RequestMethod.GET)
+    public  String updateConferenceroom(@RequestBody Conferenceroom conferenceroom){
+        int con=conferenceroomService.updateConferenceroom(conferenceroom);
+        return JSON.toJSONString(Result.success(con));
     }
 
 
 
     @RequestMapping(value="deleteConferenceroom",method = RequestMethod.DELETE)
-    public void deleteConferenceroom(@RequestBody Conferenceroom conferenceroom ){
-
+    public String deleteConferenceroom(String id ){
+        int num = conferenceroomService.deleteConferenceroom(id);
+        return JSON.toJSONString(Result.success(num));
     }
 }
